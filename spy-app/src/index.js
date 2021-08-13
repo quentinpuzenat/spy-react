943cae6f6532acfd2d7dbe907c1e5400ac6b0300
Quentin Puzenat <qpuzenat@gmail.com>
2021-08-13 14:15:39 +0200

    Added Home

--- spy-app/src/index.js
+++ spy-app/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 

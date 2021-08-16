774ef95271a90801906788712912e9fc503d94e2
Quentin Puzenat <qpuzenat@gmail.com>
2021-08-17 00:30:19 +0200

    Added routes and switch for navbar

--- spy-app/src/App.js
+++ spy-app/src/App.js
@@ -2,22 +2,28 @@ import './App.css';
 import MyNavbar from './MyNavbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Chart from './Chart';
 
 
 function App() {
   return (
+    <>
     <Router>
       <div className="App">
         <MyNavbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
+            </Route>
+            <Route path="/chart">
+              <Chart />
             </Route>
           </Switch>
         </div>
       </div>
     </Router>
+    </>
   );
 }
 
--- spy-app/src/MyNavbar.js
+++ spy-app/src/MyNavbar.js
@@ -7,16 +7,16 @@ class MyNavbar extends React.Component{
         return ( 
             <Navbar bg="dark" variant="dark" sticky="top">
                 <Container>
-                    <Navbar.Brand href="#home">SpyMe</Navbar.Brand>
+                    <Navbar.Brand href="/">SpyMe</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="/chart">EGLD Chart</Nav.Link>
                         <Nav.Link href="https://elrond.com/" target="_blank">Elrond Website</Nav.Link>
                         <Nav.Link href="https://twitter.com/big_q__" target="_blank">Contact Me</Nav.Link>
                     </Nav>
                     <Nav className="sm">
                         <Navbar.Collapse >
                             <Nav.Link>
-                            <BTCTicker /> 
+                                <BTCTicker /> 
                             </Nav.Link>
                         </Navbar.Collapse>
                     </Nav>
